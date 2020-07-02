a0f35261ecc9645ca696f5a5aaae2d076deaa516
Elismar13 <elismarsilva.5@outlook.com>
2020-07-02 20:56:34 -0300

    Directory structure update and LoginScreen added

--- App.js
+++ App.js
@@ -1,22 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    hello: {
-        fontSize: 14,
-    },
-});
+import LoginPage from './src/Pages/login';
 
 export default function App() {
     return (
-        <View styles={styles.container}>
-            <Text>Hello, World!</Text>
-        </View>
+        <LoginPage />
     );
 }
