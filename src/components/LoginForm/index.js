4d98580214d8442a79717a6e21d0aabc7e859a4c
Dywyll <scriptermatt@gmail.com>
2020-07-04 05:50:35 -0300

    Create component with arrow function.

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -3,12 +3,12 @@ import { TextInput, View } from 'react-native';
 
 import styles from './styles';
 
-export default function LoginForm(props) {
-    return (
-        // eslint-disable-next-line react/destructuring-assignment,react/prop-types
-        <View style={props.style}>
-            <TextInput elevation={5} style={styles.input} />
-            <TextInput elevation={5} style={styles.input} />
-        </View>
-    );
-}
+// eslint-disable-next-line react/prop-types
+const LoginForm = ({ style }) => (
+    <View style={style}>
+        <TextInput elevation={5} style={styles.input} />
+        <TextInput elevation={5} style={styles.input} />
+    </View>
+);
+
+export default LoginForm;
