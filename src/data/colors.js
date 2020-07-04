a35f38bdea826270342929792771189e30aec738
Elismar13 <elismarsilva.5@outlook.com>
2020-07-03 22:06:14 -0300

    Some cyberpunk style colors

---
+++ src/data/colors.js
@@ -0,0 +1,21 @@
+const colors = {
+    softLilac: '#d600ff',
+    hardLilac: '#bd00ff',
+
+    softBlue: '#00b8ff',
+    hardBlue: '#001eff',
+    darkBlue: '#091833',
+
+    softYellow: '#ffd318',
+
+    softOrange: '#ff901f',
+
+    hardRed: '#ff2975',
+    softRed: '#ff2b2b',
+
+    hardPurple: '#8c1eff',
+
+    green: '#3fff2d',
+};
+
+export default colors;
