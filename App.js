684406bee01766120c92e970f7d1386ad43b81a9
Elismar13 <elismarsilva.5@outlook.com>
2020-07-07 19:47:03 -0300

    Checking if the user has accessing application for the first time

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import Routes from './src/routes';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
-
 export default class App extends React.Component {
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstLaunch: false,
+        };
+    }
+
+    async componentDidMount() {
+        try {
+            const firstLaunch = await AsyncStorage.getItem('firstLaunch');
+            if (!firstLaunch) {
+                this.setState({ firstLaunch: true });
+            } else {
+                this.setState({ firstLaunch: false });
+            }
+        } catch (error) {
+            this.setState({ firstLaunch: true });
+        }
     }
 
     render() {
         return (
-            <View style={styles.container}>
+            <>
                 <StatusBar style="light" />
                 <Routes />
-            </View>
+            </>
         );
     }
 }
