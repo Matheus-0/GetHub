2006753d2a23f45c833069632c8c6632cfaf3a10
Elismar13 <elismarsilva.5@outlook.com>
2020-07-07 20:16:36 -0300

    First showcase screen

---
+++ src/screens/Showcase/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+export default function LoginScreen() {
+    return (
+        <View style={styles.screen}>
+            <Text style={styles.text}>Hello, Showcase!</Text>
+        </View>
+    );
+}
---
+++ src/screens/Showcase/styles.js
@@ -0,0 +1,15 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+
+    screen: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 16,
+    },
+});
+
+export default styles;
