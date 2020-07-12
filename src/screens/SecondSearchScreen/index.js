ac39051c135df156d4e5fc6d8734f9f4e7788d5a
Dywyll <scriptermatt@gmail.com>
2020-07-12 10:35:48 -0300

    Initial code for new screens.

---
+++ src/screens/SecondSearchScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+
+export default function SecondSearchScreen() {
+    return (
+        <View style={styles.screen}>
+            <Text>Initial code.</Text>
+        </View>
+    );
+}
