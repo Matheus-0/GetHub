cc219d5aea810bad12e441a9dbc972335cef2819
Elismar13 <elismarsilva.5@outlook.com>
2020-07-14 11:11:10 -0300

    Updating SearchForm to perform api requests

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,37 +1,62 @@
 import React from 'react';
 import {
-    Text, TextInput, TouchableHighlight, View,
+    Text, TextInput, TouchableHighlight, View, 
 } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Feather } from '@expo/vector-icons';
+import { getNetworkStateAsync } from 'expo-network';
+
+import { searchUsers } from '../../api/api';
 
 import styles from './styles';
 
-const SearchForm = ({ style }) => (
-    <View style={style}>
-        <View style={styles.inputView}>
-            <Feather name="search" size={28} color="black" />
-            <TextInput
-                autoCapitalize="none"
-                placeholder="Search for a repository or user"
-                style={styles.input}
-                underlineColorAndroid="transparent"
-            />
-        </View>
+class SearchForm extends React.Component {
+    constructor(props) {
+        super(props);
+        state={
+            itemName: '',
+            searchNow: false,
+        }
+    }
 
-        <TouchableHighlight
-            style={styles.touchable}
-        >
-            <LinearGradient
-                colors={['#555fa0', '#695596', '#503769']}
-                style={styles.gradientButton}
-            >
-                <Text style={styles.buttonText}>
-                    Search
-                </Text>
-            </LinearGradient>
-        </TouchableHighlight>
-    </View>
-);
+    async componentDidUpdate(prevProps, prevState) {
+        const { itemName, searchNow } = this.state;
+        if (searchNow) {
+            const users = await searchUsers(itemName);
+            this.props.navigation.navigate('SecondSearchScreen', { users });
+        }
+    }
+
+    render() {
+        return (
+            <View style={this.props.style}>
+                <View style={styles.inputView}>
+                    <Feather name="search" size={28} color="black" />
+                    <TextInput
+                        autoCapitalize="none"
+                        placeholder="Search for a repository or user"
+                        style={styles.input}
+                        underlineColorAndroid="transparent"
+                        onChangeText={(item) => this.setState({ itemName: item })}
+                    />
+                </View>
+
+                <TouchableHighlight
+                    style={styles.touchable}
+                    onPress={() => this.setState({ searchNow: true })}
+                >
+                    <LinearGradient
+                        colors={['#555fa0', '#695596', '#503769']}
+                        style={styles.gradientButton}
+                    >
+                        <Text style={styles.buttonText}>
+                            Search
+                        </Text>
+                    </LinearGradient>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+};
 
 export default SearchForm;
