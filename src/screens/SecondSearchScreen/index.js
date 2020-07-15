4e3f68deb027832f406d0f1471ba45766f411e5b
Dywyll <scriptermatt@gmail.com>
2020-07-15 11:25:57 -0300

    Gradient on search screen.

--- src/screens/SecondSearchScreen/index.js
+++ src/screens/SecondSearchScreen/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Text, View, ScrollView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import styles from './styles';
+import colors from '../../data/colors';
 
 export default function SecondSearchScreen({ route, navigation }) {
     const { users } = route.params;
 
     return (
         <View style={styles.screen}>
+            <LinearGradient
+                colors={['#1e1e1e', '#0f0f0f']}
+                start={[0.6, 0.3]}
+                end={[0.3, 0.7]}
+                style={styles.gradient}
+            />
+
             <ScrollView contentContainerStyle={styles.scrollView}>
                 {users.items.map((user) => (
                     <View style={styles.userView} key={user.id}>
--- src/screens/SecondSearchScreen/styles.js
+++ src/screens/SecondSearchScreen/styles.js
@@ -1,6 +1,13 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    gradient: {
+        height: Dimensions.get('window').height,
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+    },
     screen: {
         flex: 1,
         justifyContent: 'center',
