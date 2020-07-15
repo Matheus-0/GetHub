ad40630378c247f3578ca49fcc42cba220d08bc4
Dywyll <scriptermatt@gmail.com>
2020-07-15 15:25:50 -0300

    Update list by typing.

--- src/screens/SecondSearchScreen/index.js
+++ src/screens/SecondSearchScreen/index.js
@@ -1,35 +1,79 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import {
+    ScrollView, Text, TextInput, View,
+} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { Feather } from '@expo/vector-icons';
 
 import UserContainer from '../../components/UserContainer';
 
 import styles from './styles';
+import { searchUsers } from '../../api/api';
 
-export default function SecondSearchScreen({ route, navigation }) {
-    const { users } = route.params;
+export default class SecondSearchScreen extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <View style={styles.screen}>
-            <LinearGradient
-                colors={['#1e1e1e', '#0f0f0f']}
-                start={[0.6, 0.3]}
-                end={[0.3, 0.7]}
-                style={styles.gradient}
-            />
+        this.state = {
+            query: '',
+            users: this.props.route.params.users,
+        };
+    }
 
-            <Text style={styles.searchTitle}>Search results</Text>
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        await this.updateUserList();
+    }
 
-            <ScrollView contentContainerStyle={styles.scrollView}>
-                {users.items.map((user) => (
-                    <UserContainer
-                        gradientStyle={styles.gradientStyle}
-                        key={user.id}
-                        touchableStyle={styles.touchableStyle}
-                        user={user}
-                    />
-                ))}
-            </ScrollView>
-        </View>
-    );
+    async updateUserList() {
+        const { query } = this.state;
+
+        if (query.trim()) {
+            const users = await searchUsers(query);
+
+            if (users) this.setState({ users });
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.screen}>
+                <LinearGradient
+                    colors={['#1e1e1e', '#0f0f0f']}
+                    start={[0.6, 0.3]}
+                    end={[0.3, 0.7]}
+                    style={styles.gradient}
+                />
+
+                <View style={styles.searchInputView}>
+                    <View style={styles.inputView}>
+                        <Feather name="search" size={28} color="black" />
+                        <TextInput
+                            autoCapitalize="none"
+                            placeholder="Search for a repository or user"
+                            style={styles.input}
+                            underlineColorAndroid="transparent"
+                            onChangeText={(query) => {
+                                this.setState({ query });
+                            }}
+                        />
+                    </View>
+                </View>
+                <View style={styles.searchTitleView}>
+                    <Text style={styles.searchTitle}>Search results</Text>
+                </View>
+                <View style={styles.scrollViewParent}>
+                    <ScrollView contentContainerStyle={styles.scrollView}>
+                        {this.state.users.items.map((user) => (
+                            <UserContainer
+                                gradientStyle={styles.gradientStyle}
+                                key={user.id}
+                                touchableStyle={styles.touchableStyle}
+                                user={user}
+                            />
+                        ))}
+                    </ScrollView>
+                </View>
+            </View>
+        );
+    }
 }
