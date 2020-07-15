a8a52328e3855558c667e4876ca879eb5141c40b
Dywyll <scriptermatt@gmail.com>
2020-07-15 11:30:33 -0300

    Initial UserContainer code.

---
+++ src/components/UserContainer/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './styles';
+
+export default class UserContainer extends React.Component {
+    render() {
+        return super.render();
+    }
+}
