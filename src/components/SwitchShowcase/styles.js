28ba5388f4f296c55c3b58fe391200685b4cd82e
Elismar13 <elismarsilva.5@outlook.com>
2020-07-22 20:03:27 -0300

    Adding padding to the buttons View

--- src/components/SwitchShowcase/styles.js
+++ src/components/SwitchShowcase/styles.js
@@ -3,8 +3,10 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     buttons: {
         flexDirection: 'row',
-        justifyContent: 'space-around',
+        justifyContent: 'space-between',
         marginTop: 15,
+        paddingLeft: 20,
+        paddingRight: 20,
         width: '100%',
     },
 });
