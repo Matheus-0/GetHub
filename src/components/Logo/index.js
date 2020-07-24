1f83153f86f145c03a8c1e2d247bb268058216e6
Elismar13 <elismarsilva.5@outlook.com>
2020-07-23 21:28:01 -0300

    isHeader logo style logic

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { View, Text, Image } from 'react-native';
 import styles from './styles';
 
-export default function Logo({ style }) {
+export default function Logo({ style, isHeader = false }) {
     return (
         <Image
-            style={styles.normal}
+            style={isHeader ? styles.header : styles.normal}
+            // eslint-disable-next-line global-require
             source={require('../../../assets/logo.png')}
         />
     );
