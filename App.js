f3cb5a92d8a4cb45cdaf70c1e5648dd6f6b64a5c
Elismar13 <elismarsilva.5@outlook.com>
2020-07-29 21:19:02 -0300

    Translucent prop disabled on the status bar

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ export default class App extends React.Component {
 
         return (
             <>
-                <StatusBar style="light" />
+                <StatusBar style="light" translucent={false} />
                 <Routes isFirstLaunch={firstLaunch} networkAvailable={networkAvailable} />
             </>
         );
