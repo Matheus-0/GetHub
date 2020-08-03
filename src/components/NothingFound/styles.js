8e59d3fa3b832d3d13b33aed2910cde48d5a578b
Elismar13 <elismarsilva.5@outlook.com>
2020-08-03 12:34:26 -0300

    NothingFound component fixes

--- src/components/NothingFound/styles.js
+++ src/components/NothingFound/styles.js
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
+        justifyContent: 'center',
         flex: 1,
         padding: 20,
     },
